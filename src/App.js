97cd1191f80563968ab9f45e50498926b8c824c3
NgoSonTungDev <ngotungdev@gmail.com>
2022-04-25 16:19:53 +0700

    ...

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+import { BrowserRouter ,Redirect,Route ,Switch} from "react-router-dom";
+import Login from "./pages/Login/Login";
+import NotFound from "./pages/NotFound/NotFound";
+import Register from "./pages/Register/Register";
 
 function App() {
   return (
-    <div className="App">
-      ngosontung
-    </div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Login} />
+          <Route path="/registerUser" exact component={Register} />
+          <Route path="/notfound" component={NotFound} />
+          <Redirect to="/notfound" />
+        </Switch>
+      </BrowserRouter>
   );
 }
 

f21e39be79bba97423a1815ffa138432c3971a8a
NgoSonTungDev <ngotungdev@gmail.com>
2022-04-29 12:08:11 +0700

    design page home

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import { BrowserRouter ,Redirect,Route ,Switch} from "react-router-dom";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import NotFound from "./pages/NotFound/NotFound";
 import Register from "./pages/Register/Register";
 
+
 function App() {
   return (
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" exact component={Login} />
-          <Route path="/home" exact component={Home} />
-          <Route path="/registerUser" exact component={Register} />
-          <Route path="/notfound" component={NotFound} />
-          <Redirect to="/notfound" />
-        </Switch>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Login} />
+        <Route path="/home" exact component={Home} />
+        <Route path="/registerUser" exact component={Register} />
+        <Route path="/notfound" component={NotFound} />
+        <Redirect to="/notfound" />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
